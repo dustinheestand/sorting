4dd4db001ac38d52ddf194855d6ffa220c059482
dustinheestand <heestand@post.harvard.edu>
2018-07-30 14:28:30 -0500

    mergesort is broken

--- mergesort.js
+++ mergesort.js
@@ -1,9 +1,22 @@
 function split(arr) {
-    const firstHalf = arr.slice(0,Math.floor(arr.length / 2));
-    const secondHalf = arr.slice(Math.floor(arr.length / 2));
-    return [firstHalf, secondHalf];
+  const firstHalf = arr.slice(0, Math.floor(arr.length / 2));
+  const secondHalf = arr.slice(Math.floor(arr.length / 2));
+  return [firstHalf, secondHalf];
 }
 
-function merge(){
-    
+function merge([first, second]) {
+  const mergedArray = [];
+  while (first.length > 0 && second.length > 0) {
+    if (first[0] < second[0]) {
+      mergedArray.push(first.shift());
+    } else {
+      mergedArray.push(second.shift());
+    }
+  }
+  return mergedArray.concat(first, second);
+}
+
+function mergeSort(arr) {
+  if (arr.length == 1) return arr;
+  else return merge(mergeSort(split(arr)));
 }
--- mergespec.spec.js
+++ mergespec.spec.js
@@ -1,8 +1,27 @@
 describe("Merge Sort", function() {
-    it("has a split function that splits an array", function(){
-        expect(split([1,2,3,4])).toEqual([[1,2],[3,4]])
-    })
-    it("has merge function that merges two arrays", function(){
-        expect(merge([[1,2], [3,4]])).toEqual([[1,2,3,4]]);
-    })
-})
+  let arrayForSorting = [];
+
+  beforeAll(function() {
+    for (let i = 0; i < 20; i++) {
+      arrayForSorting.push(Math.random());
+    }
+    spyOn(window, "mergeSort").and.callThrough();
+    mergeSort(arrayForSorting);
+  });
+
+  it("has a split function that splits an array", function() {
+    expect(split([1, 2, 3, 4])).toEqual([[1, 2], [3, 4]]);
+  });
+  it("has merge function that merges two arrays", function() {
+    expect(merge([[1, 2], [3, 4]])).toEqual([1, 2, 3, 4]);
+  });
+  it("can sort a random array", function() {
+    expect(mergeSort(arrayForSorting)).toEqual(arrayForSorting.sort());
+  });
+  it(`calls itself recursively; this doesn't really 
+      test for that but we're trying`, function() {
+    expect(window.mergeSort.calls.count).toBeGreaterThan(
+      arrayForSorting.length
+    );
+  });
+});
