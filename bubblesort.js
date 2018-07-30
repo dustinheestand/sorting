4b0dfe9c7deaff3e8334a18c6916dd985e077ae0
pindimindi <milicarodic307@yahoo.com>
2018-07-30 11:16:31 -0500

    loops

---
+++ bubblesort.js
@@ -0,0 +1,21 @@
+function bubbleSort(arr) {
+    //return [];
+    let size = arr.length;
+    //while unsorted
+    while (size > 1) {
+        for (let i = 0; i < size; i++) {
+
+
+        }
+        size--;
+    }
+
+
+}
+
+function compare(a,b) {
+
+
+}
+
+function 
